6170548a5a0288372588b1a6e2c9d0da299692d5
davidtc8 <davidtorresc8@gmail.com>
2020-12-26 18:56:42 -0600

    File where contains the food class

--- food.py
+++ food.py
@@ -9,8 +9,14 @@ class Food(Turtle):
         self.shape('circle')
         self.penup()
         self.shapesize(stretch_len= .5, stretch_wid= .5)
-        self.color('white')
+        self.color('yellow')
         self.speed('fastest')
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint (-280, 280)
+        random_y = random.randint (-280, 280)
+        self.goto (random_x, random_y)
