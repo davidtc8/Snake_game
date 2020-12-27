ef11ffdfe1efd17bc356a54a0cdbfb30db7e98b6
davidtc8 <davidtorresc8@gmail.com>
2020-12-26 18:39:06 -0600

    Food Class for the Snake Class

---
+++ food.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+# Create our food class which is inherited from the Turtle Class
+# Now, this food is like when we create our Turtle but we're customizing it.
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len= .5, stretch_wid= .5)
+        self.color('white')
+        self.speed('fastest')
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
